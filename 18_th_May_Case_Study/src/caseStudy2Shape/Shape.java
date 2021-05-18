344aab68119462fd3d98f4abf178567e5cdfa8c4
Sonarika Das <63847726+Sonarika-Das@users.noreply.github.com>
2021-05-18 23:31:45 +0530

    Add files via upload

---
+++ 18_th_May_Case_Study/src/caseStudy2Shape/Shape.java
@@ -0,0 +1,29 @@
+package caseStudy2Shape;
+
+public class Shape 
+{
+	
+	String color;
+	boolean filled;
+	
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	
+	public boolean isFilled() {
+		return filled;
+	}
+	public void setFilled(boolean filled) {
+		this.filled = filled;
+	}
+	
+	@Override
+	public String toString() {
+		return "Shape[color="+this.color+",filled="+this.filled+"]";
+	}
+	
+
+}
