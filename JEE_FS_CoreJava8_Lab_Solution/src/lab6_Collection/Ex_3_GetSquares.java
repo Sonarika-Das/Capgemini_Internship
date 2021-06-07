4ae107fe68cd4412c8d82144de7eae32deb3d768
Sonarika Das <63847726+Sonarika-Das@users.noreply.github.com>
2021-06-07 07:32:19 +0530

    Add files via upload

---
+++ JEE_FS_CoreJava8_Lab_Solution/src/lab6_Collection/Ex_3_GetSquares.java
@@ -0,0 +1,36 @@
+package com.capgemini.lab6_Collection;
+
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Ex_3_GetSquares {
+
+	private void getSquares(int[] arr) {
+		
+		HashMap<Integer,Integer>hmap = new HashMap<Integer,Integer>();
+		
+		for(int itr : arr) hmap.put(itr, itr*itr);
+		
+		for(Map.Entry<Integer, Integer> e : hmap.entrySet())
+			System.out.println(e.getKey() + " \t- " + e.getValue());
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter the size of array: "); int size = sc.nextInt();
+		int[] arr = new int[size];
+		System.out.println("Enter " + size + " elements in the array: ");
+		for(int i = 0; i < arr.length; i++)
+			arr[i] = sc.nextInt();
+	
+		new Ex_3_GetSquares().getSquares(arr);
+		sc.close();
+	}
+
+	
+}
---
+++ JEE_FS_CoreJava8_Lab_Solution/src/lab6_Collection/Ex_5_SecondSmallestInteger.java
@@ -0,0 +1,34 @@
+package com.capgemini.lab6_Collection;
+
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Ex_5_SecondSmallestInteger {
+
+
+	private static int getSecondSmallest(int[] arr) {
+		
+		ArrayList<Integer> arrayList  = new ArrayList<Integer>();
+		for(int element: arr) arrayList.add(element);
+		
+		Collections.sort(arrayList);
+		
+		return arrayList.get(1);
+	}
+	
+	public static void main(String[] args) {
+	
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the size of array: ");
+		int n = sc.nextInt(), arr[] = new int[n];
+		System.out.println("Enter " + n + " elements in the array: ");
+		for(int i = 0; i<arr.length; i++) arr[i]= sc.nextInt();
+		
+		System.out.println("The second smallest integer in the array: " + getSecondSmallest(arr));
+		sc.close();
+		
+	}
+
+}
